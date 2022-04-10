330d9d26e66b6a18ff15df7946ff509140151842
Manlio Marchica <manlio.marchica@euei.it>
2022-04-10 15:50:04 +0200

    continue auth module with dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, ParseIntPipe, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signup() {
+//    signup(@Body('email') email: string, @Body('password', ParseIntPipe) password: string) {
+      signup(@Body() dto: AuthDto) {  
+// ParseIntPipe è una pipe di sistema, qui usata solo per far vedere un uso come validatore di input
+      
        return this.authService.signup();
     }
 
