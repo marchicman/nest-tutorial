31ebbc5f3d395bb8db49931398de8fe77d363fb0
Manlio Marchica <manlio.marchica@euei.it>
2022-04-13 09:09:58 +0200

    add migtation unique field email e relations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { userInfo } from "os";
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-    signup(dto: AuthDto) {
-        return {
-            msg: 'I am signed up'
-        };
+    async signup(dto: AuthDto) {
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+        // save the user in the db
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+            // select: {
+            //     id: true,
+            //     email: true,
+            //     createdAt: true
+            // }
+        });
+        delete user.hash; // dirty solution in attesa di parlare di transformer
+
+        // return the saved user
+        return user;
     }
 
     signin() {
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
+    whitelist: true, // elimina dal body della richiesta eventuali campi non presenti nel DTO del controller
   }));
   await app.listen(3333);
 }
