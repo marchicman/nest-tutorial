8552c6f939f7a67e238c539ca877df91cbaa2e45
Manlio Marchica <manlio.marchica@euei.it>
2022-04-15 08:52:29 +0200

    gestione email presente duplicate key

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { userInfo } from "os";
+import { Prisma } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
@@ -10,22 +12,32 @@ export class AuthService {
     async signup(dto: AuthDto) {
         // generate the password hash
         const hash = await argon.hash(dto.password);
-        // save the user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-            // select: {
-            //     id: true,
-            //     email: true,
-            //     createdAt: true
-            // }
-        });
-        delete user.hash; // dirty solution in attesa di parlare di transformer
+        try {
+            // save the user in the db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+                // select: {
+                //     id: true,
+                //     email: true,
+                //     createdAt: true
+                // }
+            });
+            delete user.hash; // dirty solution in attesa di parlare di transformer
 
-        // return the saved user
-        return user;
+            // return the saved user    
+            return user;
+        } catch(error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    // duplicate key
+                    throw new ForbiddenException('Credentials taken');
+                }
+            }
+            throw error;
+        }
     }
 
     signin() {
